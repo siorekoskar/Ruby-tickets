cfc9c88b8f70670ce8c31717eaa9f94886cdd04c
Siorek, Oskar <oskar.siorek@sabre.com>
2020-01-15 21:29:27 +0100

    .

---
+++ db/migrate/20200115202633_add_phone_to_tickets.rb
@@ -0,0 +1,4 @@
+class AddPhoneToTickets < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_193032) do
+ActiveRecord::Schema.define(version: 2020_01_15_202633) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
