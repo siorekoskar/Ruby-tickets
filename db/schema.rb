26b2bc55ee5c0c1e6894302d017a813b1a37543b
Siorek, Oskar <oskar.siorek@sabre.com>
2020-01-15 23:12:43 +0100

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_204354) do
+ActiveRecord::Schema.define(version: 2020_01_15_221059) do
 
   create_table "events", force: :cascade do |t|
     t.string "artist"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_01_15_204354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone"
+    t.integer "event_id"
+    t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
+  add_foreign_key "tickets", "events"
 end
