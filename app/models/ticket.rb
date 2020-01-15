989dba818ecf7cf43d52163ee63c614bfc96b263
Siorek, Oskar <oskar.siorek@sabre.com>
2020-01-15 21:24:00 +0100

    .

---
+++ app/models/ticket.rb
@@ -0,0 +1,7 @@
+class Ticket < ApplicationRecord
+    validates :name, :presence => true, :length => { :minimum => 6 }
+    validates :email_address, :presence => true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :price, :presence => true
+    validates :address, :presence => true
+    validates :seat_id_seq, :presence => true
+end
