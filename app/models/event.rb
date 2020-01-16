5c128c1e9a91ec2021db536ee986e947af1443d9
Siorek, Oskar <oskar.siorek@sabre.com>
2020-01-16 03:03:54 +0100

    .

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,19 @@
-class Event < ApplicationRecord
+class Event < ActiveRecord::Base
     validates :artist, :presence => true
     validates :description, :presence => true
     validates :price_low, :presence => true, :numericality =>true
     validates :price_high, :presence => true, :numericality =>true
-    # validates :event_date, :presence => true
+    validates :event_date, :presence => true
+
+    validate :event_date_not_from_past
+    private
+    def event_date_not_from_past
+        puts "CIPKAA"
+        if event_date < Date.today
+            errors.add('Data wydarzenia', 'nie może być z przeszłości')
+        end
+    end
 
     has_many :tickets
 end
+
