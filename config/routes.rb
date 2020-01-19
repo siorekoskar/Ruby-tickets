ddc291422886e1229ed877d68bcadf4bef95f520
Siorek, Oskar <oskar.siorek@sabre.com>
2020-01-19 17:17:14 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :events, :only => [:index, :new, :create, :show]
   # get 'events/new' => 'events#new'
   # get 'events/create'
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   # post '/events' => 'events#create'
   root :to => "tickets#index"
   resources :tickets
+  devise_for :users
+devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
